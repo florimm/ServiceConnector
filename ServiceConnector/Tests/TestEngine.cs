0000a486a615250087b2d40d050e1e35c911697e
Florim Maxhuni <fmaxhuni>
2015-12-27 23:09:29 +0100

    WebApi added

--- ServiceConnector/Tests/TestEngine.cs
+++ ServiceConnector/Tests/TestEngine.cs
@@ -8,18 +8,6 @@ using ServiceConnector.Contracts;
 
 namespace Tests
 {
-    //[Subject("Executor test")]
-    //public class when_exucutor_execute_with_no_command_throws_exception
-    //{
-    //    private static Executor _executor; 
-    //    Establish context = () =>
-    //    {
-    //        _executor = new Executor();
-    //    };
-    //    Because of = () => _executor.Test();
-
-    //    It should_not_be_null = () => _executor.ShouldNotBeNull();
-    //}
     [TestFixture]
     public class TestEngine
     {
